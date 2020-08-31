6be0a16aa710933e3d4fdc2d86afd81dc88ffcb9
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2020-08-31 15:57:51 +0500

    пофиксин ShowInfo.php

--- task_2.2/app/Services/ShowInfo.php
+++ task_2.2/app/Services/ShowInfo.php
@@ -8,7 +8,8 @@ class ShowInfo
 {
     public function showInfo(): void
     {
+        $name = config('app.name');
         echo 'Текущее окружение: ' . \App::environment() . PHP_EOL;
-        echo 'Ваше имя: ' . env('NAME') . PHP_EOL;
+        echo 'Ваше имя: ' . $name . PHP_EOL;
     }
 }
